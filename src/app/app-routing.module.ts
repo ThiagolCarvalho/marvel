2dbcf1b49bd4d09d2cd910f1dda0969591ac0810
thiago <thiagol.carvalho97@gmail.com>
2021-10-01 01:36:47 -0300

    Fixed images being cropped, added route for characters page to be the root page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 
 const routes: Routes = [
-  { path: 'characters', loadChildren: () => import('./pages/characters/characters.module').then(m => m.CharactersModule)}
+  { path: 'characters', loadChildren: () => import('./pages/characters/characters.module').then(m => m.CharactersModule)},
+  { path: '', redirectTo: '/characters', pathMatch: 'full'}
 ]
 
 @NgModule({
