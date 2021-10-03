3815a4e0abbc20e5568806baa67e247aa81c38b7
thiago <thiagol.carvalho97@gmail.com>
2021-10-03 12:53:37 -0300

    Added header component with router links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 import {FooterComponent} from './shared/components/footer/footer.component';
+import {HeaderComponent} from "./shared/components/header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
